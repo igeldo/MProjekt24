7becf4006f9375cec4589b2c15297baeb73e3c55
EmiSok201 <152559054+EmiSok201@users.noreply.github.com>
2024-02-07 21:39:39 +0100

    Delete Schaltung1 directory

--- Schaltung1/Schaltung/Bauelemente/PowerSource.py
+++
@@ -1,9 +0,0 @@
-class PowerSource:
-
-    def __init__(self, ampere):
-        self._ampere = ampere
-
-    def get_ampere(self):
-        """Return ampere value."""
-        ampere = self._ampere
-        return ampere
