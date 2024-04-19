a61692dd7dfacca798644cf7f4c23292e91e620c
ig <igel@fh-dortmund.de>
2024-04-19 15:18:38 +0200

    first commit

---
+++ z_main.py
@@ -0,0 +1,14 @@
+
+
+
+
+class Main:
+    def run(self):
+        t = "example"
+        i = 42
+        print(f"to print  {t} ... {i}")
+
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
