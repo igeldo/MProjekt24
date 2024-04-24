2d472b3439a80153a4ea584e8faa929641623752
Georg Pietrek <georg.pietrek@conciso.de>
2024-04-24 17:51:19 +0200

    add y_main.py

---
+++ y_main.py
@@ -0,0 +1,14 @@
+
+
+
+
+class Main:
+    def run(self):
+        t = "example"
+        i = 42
+        print(f"Georgs print  {t} ... {i}")
+
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
