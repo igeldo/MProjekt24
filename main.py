e9b624b942d440a6f82f93efe8e25428a6c6b7b5
Cigdem <cigdem.bayrak001@stud.fh-dortmund.de>
2024-05-06 14:17:31 +0200

    Backslash between Person 1 and Person 2

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ class Main:
     def run(self):
         person1 = Person('Harald', 'Mueller', 'xxx', 'HM')
         person1.show_attributes()
+        print('\n')
         person2 = Person('Steffanie', 'Vogel', 'xxx', 'SV')
         person2.show_attributes()
 
