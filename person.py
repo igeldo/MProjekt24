ecc842864edb88b1eebba1145ed6589721e6deb9
Cigdem <cigdem.bayrak001@stud.fh-dortmund.de>
2024-05-06 14:29:39 +0200

    Backslash between Person 1 and Person 2

--- person.py
+++ person.py
@@ -1,18 +1,31 @@
 from abc import abstractmethod, ABC
 
+from datetime import date
+
 
 class Person:
-    def __init__(self, a, b, c, d):
-        self._name = a
-        self._surname = b
-        self._birthdate = c
-        self._abbreviation = d
+    def __init__(self, name, surname, birthdate, abbreviation):
+        self._name = name
+        self._surname = surname
+        self._birthdate = birthdate
+        self._abbreviation = abbreviation
 
     def show_attributes(self):
-        print('Vorname: ' + self._name)
-        print('Nachname: ' + self._surname)
-        print('Geburtsdatum: ' +self._birthdate)
-        print('Kürzel: ' +self._abbreviation)
+        print('Vorname:', self._name)
+        print('Nachname:', self._surname)
+        print('Geburtsdatum:', self._birthdate)
+        print('Kürzel:', self._abbreviation)
+
+    def calculate_age(self):
+        today = date.today()
+        birthdate = date.fromisoformat(self._birthdate)
+        age = today.year - birthdate.year
+
+        # Überprüfen, ob der Geburtstag in diesem Jahr noch nicht stattgefunden hat
+        if (today.month, today.day) < (birthdate.month, birthdate.day):
+            age -= 1
+
+        return age
 
     def neu_anlegen(self):
         # Hier können Sie den Code einfügen, um eine neue Person anzulegen
