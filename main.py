d45e059c32aa6ce8df11703940ea1a219e96372d
JasperEis <jasper.eisenberg003@stud.fh-dortmund.de>
2024-05-06 14:08:01 +0200

    changes in Main: First Person Harald

---
+++ main.py
@@ -0,0 +1,12 @@
+from person import Person
+
+
+class Main:
+    def run(self):
+        Person1 = Person(Harald, Mueller, xxx, HM)
+        Person1.show_attributes()
+
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
--- person.py
+++ person.py
@@ -1,13 +1,18 @@
 from abc import abstractmethod, ABC
 
 
-
 class Person:
     def __init__(self, a, b, c, d):
         self._name = a
         self._surname = b
         self._birthdate = c
         self._abbreviation = d
+
+    def show_attributes(self):
+        print(self._name)
+        print(self._surname)
+        print(self._birthdate)
+        print(self._abbreviation)
 
     def neu_anlegen(self):
         # Hier können Sie den Code einfügen, um eine neue Person anzulegen
