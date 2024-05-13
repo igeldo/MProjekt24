fff058ef1c1fd04a6d885881739377262e68963d
JasperEis <jasper.eisenberg003@stud.fh-dortmund.de>
2024-05-13 10:24:01 +0200

    edit doctor class, add Fachrichtunf

--- doctor.py
+++ doctor.py
@@ -1,6 +1,11 @@
 from person import Person
 
 class Doctor(Person):
-    def __init__(self, profession: str):
-       # super().__init__( name, surname, birthdate, abbreviation, age)
+    def __init__(self, name: str, surname: str, birthdate: str, abbreviation: str, profession: str):
+        super().__init__(name, surname, birthdate, abbreviation)
         self._profession = profession
+
+    def show_attributes(self):
+        super().show_attributes()
+        print('Fachrichtung:', self._profession)
+
