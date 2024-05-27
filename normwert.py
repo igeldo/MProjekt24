d79e6ed0c8c97eee74814071a8197f0ca93b7ee9
JasperEis <jasper.eisenberg003@stud.fh-dortmund.de>
2024-05-27 12:00:01 +0200

    changed view

--- normwert.py
+++ normwert.py
@@ -14,7 +14,18 @@ class Normwert:
 class Normwerte(Enum):
     HaemoglobinMale = Normwert('HB', 'mmol/l', 8.1, 10.5, 'transportiert Sauerstoff im Blut')
     HaemoglobinFemale = Normwert('HB', 'mmol/l', 7.4, 9.9, 'transportiert Sauerstoff im Blut')
-    LeukozytenMale = Normwert('WBC', '/ul', 4000, 10000, 'wichtig für Immunsysthem')
+    LeukozytenMale = Normwert('WBC', '/u"\u03bcs"', 4000, 10000, 'wichtig für Immunsysthem')
     LeukozytenFemale = LeukozytenMale
-    ErythrozytenMale = Normwert('RBC', 'Mio/ul', 4.5, 5.9, 'zuständig für Sauerstofftransport')
-    ErythrozytenFemale = Normwert('RBC', 'Mio/ul', 4.1, 5.2, 'zuständig für Sauerstofftransport')
+    ErythrozytenMale = Normwert('RBC', 'Mio/u"\u03bcs"', 4.5, 5.9, 'zuständig für Sauerstofftransport')
+    ErythrozytenFemale = Normwert('RBC', 'Mio/u"\u03bcs"', 4.1, 5.2, 'zuständig für Sauerstofftransport')
+    ThrombozytenMale = Normwert('PLT', '/u"\u03bcl"', 1500000, 400000, 'Blutplättchen, zuständig für Blutgerinnung')
+    ThrombozytenFemale = ThrombozytenMale
+    HämatokritMale = Normwert('HCT', '%', 42,50,'feste Bestandteile im Blut')
+    HämatokritFemale = Normwert('HCT', '%', 38,44,'feste Bestandteile im Blut')
+    GlukoseMale = Normwert('GLC', 'mg/ dl', 70, 100, 'maßgeblich für die Energieversorgung')
+    GlukoseFemale = GlukoseMale
+    CProteinMale = Normwert('CRP', 'mg/ dl', 'none', 0.5,'Entzündungsmarker')
+    CProteinFemale = CProteinMale
+    KreatininMale = Normwert('KR', 'mg/ dl',0.9 , 1.3,'Aussage zur Nierenfunktion')
+    KreatininFemale = Normwert('KR', 'mg/ dl',0.6 , 1.1,'Aussage zur Nierenfunktion')
+
--- personView.py
+++ personView.py
@@ -1,3 +1,5 @@
+from doctor import Doctor
+from patient import Patient
 class PersonView:
     def display_person(self, person):
         if isinstance(person, Patient):
@@ -8,27 +10,27 @@ class PersonView:
             self.display_generic_person(person)
 
     def display_patient(self, patient):
+        print(f"Patient ID: {patient.get_patient_id()}")
         print(f"Name: {patient.get_name()}")
         print(f"Surname: {patient.get_surname()}")
         print(f"Birthdate: {patient.get_birthdate()}")
-        print(f"Phone Number: {patient.get_phone_number()}")
         print(f"Abbreviation: {patient.get_abbreviation()}")
         print(f"Age: {patient.get_age()}")
+        print(f"Sex: {patient.get_sex()}")
+        print(f"Phone Number: {patient.get_phone_number()}")
         print(f"Pre-Illness: {patient.get_preillness()}")
         print(f"Symptoms: {patient.get_symptoms()}")
-        print(f"Sex: {patient.get_sex()}")
-        print(f"Patient ID: {patient.get_patient_id()}")
         print('\n')
 
     def display_doctor(self, doctor):
+        print(f"Title: {doctor.get_title()}")
         print(f"Name: {doctor.get_name()}")
         print(f"Surname: {doctor.get_surname()}")
-        print(f"Birthdate: {doctor.get_birthdate()}")
-        print(f"Phone Number: {doctor.get_phone_number()}")
         print(f"Abbreviation: {doctor.get_abbreviation()}")
+        print(f"Birthdate: {doctor.get_birthdate()}")
         print(f"Age: {doctor.get_age()}")
+        print(f"Phone Number: {doctor.get_phone_number()}")
         print(f"Profession: {doctor.get_profession()}")
-        print(f"Title: {doctor.get_title()}")
         print('\n')
 
     def display_generic_person(self, person):
