bc681e624d86b271e6852eac6156c39995ccc445
Cigdem <cigdem.bayrak001@stud.fh-dortmund.de>
2024-05-27 11:23:21 +0200

    no problems anymore

---
+++ personView.py
@@ -0,0 +1,8 @@
+class PersonView:
+    def display_person(self, person):
+        print(f"Name: {person.getName()}")
+        print(f"Surname: {person.getSurname()}")
+        print(f"Birthdate: {person.getBirthdate()}")
+        print(f"Phone Number: {person.getPhoneNumber()}")
+        print(f"Abbreviation: {person.getAbbreviation()}")
+        print(f"Age: {person.getAge()}")
