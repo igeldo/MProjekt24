9cbdc2bca5bf44765b95a7254e992ced8dedab2b
Cigdem <cigdem.bayrak001@stud.fh-dortmund.de>
2024-05-27 11:19:36 +0200

    no problems anymore

---
+++ class PersonController.py
@@ -0,0 +1,11 @@
+class PersonController:
+    def __init__(self):
+        self.persons = []
+
+    def add_person(self, person):
+        self.persons.append(person)
+
+    def display_persons(self):
+        view = PersonView()
+        for person in self.persons:
+            view.display_person(person)
